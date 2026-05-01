48fc73f801a7ad21b1e91d6ca4cbc93e505e1f71
Tanisha <tthompsonjones8771@gmail.com>
2026-04-30 22:12:51 -0400

    (feat/express): create reminder comments in server.ts file

--- backend/server.ts
+++ backend/server.ts
@@ -11,3 +11,14 @@ app.use(express.json());
 
 //! TODO: Create express.static(path.resolve(import.meta.dirname, '../dist')) middleware. 
 
+
+//! TODO: Add global error handler middleware (must be after all routes)
+
+
+//! Start server
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+});
+
+// ? Don't forget to export the app
+export default app;
