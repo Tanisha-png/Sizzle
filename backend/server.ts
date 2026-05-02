d4d3381bfbf3b708eec95075f33de9bc33bb193e
Tanisha <tthompsonjones8771@gmail.com>
2026-05-02 12:55:27 -0400

    (server): add fake data in server.ts for testing

--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,43 @@
+import "dotenv/config";
 import path from "path";
 import express from "express";
 import mongoose from "mongoose";
-import 'dotenv/config';
-
+import Recipe from "./models/Recipe";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-//! TODO: Create express.static(path.resolve(import.meta.dirname, '../dist')) middleware. 
+const dbUri = process.env.MONGO_URI;
 
+if (!dbUri) {
+    console.error("❌ Error: MONGO_URI is not defined in the .env file.");
+    process.exit(1); // Stop the server if there's no database string
+}
+
+mongoose.connect(dbUri!).then(async () => {
+    console.log("✅ Success: Sizzle is connected to MongoDB Atlas!");
+
+    // --- TEST SEEDING START ---
+    try {
+        const testRecipe = new Recipe({
+        title: "Sizzling Garlic Shrimp",
+        ingredients: ["Shrimp", "Garlic", "Olive Oil", "Red Pepper Flakes"],
+        instructions:
+            "Sauté garlic in oil, add shrimp until pink, sprinkle flakes.",
+        cookTime: 10,
+    });
+
+    const savedRecipe = await testRecipe.save();
+    console.log("🚀 Test Recipe Saved:", savedRecipe.title);
+    } catch (err) {
+        console.error("❌ Error saving test recipe:", err);
+    }
+  // --- TEST SEEDING END ---
+});
+
+//! TODO: Create express.static(path.resolve(import.meta.dirname, '../dist')) middleware.
 
 //! TODO: Add global error handler middleware (must be after all routes)
 
@@ -20,7 +47,7 @@ app.use(express.json());
 
 //! Start server
 app.listen(3000, () => {
-    console.log("🔥 Server is sizzling on port 3000");
+  console.log("🔥 Server is sizzling on port 3000");
 });
 
 // ? Don't forget to export the app
